b94d8a2b66f14d0a7957405573dbbd77c0ea7295
paul wisehart <paul@oldcode.org>
2018-04-16 00:24:39 +0000

    reqrep example from go-mangos to start

--- cmd/nmserver/main.go
+++ cmd/nmserver/main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-mangos/mangos"
+	"github.com/go-mangos/mangos/protocol/rep"
+	"github.com/go-mangos/mangos/protocol/req"
+	"github.com/go-mangos/mangos/transport/ipc"
+	"github.com/go-mangos/mangos/transport/tcp"
+	"os"
+	"time"
+)
+
+func die(format string, v ...interface{}) {
+	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, v...))
+	os.Exit(1)
+}
+
+func date() string {
+	return time.Now().Format(time.ANSIC)
+}
+
+func node0(url string) {
+	var sock mangos.Socket
+	var err error
+	var msg []byte
+	if sock, err = rep.NewSocket(); err != nil {
+		die("can't get new rep socket: %s", err)
+	}
+	sock.AddTransport(ipc.NewTransport())
+	sock.AddTransport(tcp.NewTransport())
+	if err = sock.Listen(url); err != nil {
+		die("can't listen on rep socket: %s", err.Error())
+	}
+	for {
+		// Could also use sock.RecvMsg to get header
+		msg, err = sock.Recv()
+		if string(msg) == "DATE" { // no need to terminate
+			fmt.Println("NODE0: RECEIVED DATE REQUEST")
+			d := date()
+			fmt.Printf("NODE0: SENDING DATE %s\n", d)
+			err = sock.Send([]byte(d))
+			if err != nil {
+				die("can't send reply: %s", err.Error())
+			}
+		}
+	}
+}
+
+func node1(url string) {
+	var sock mangos.Socket
+	var err error
+	var msg []byte
+
+	if sock, err = req.NewSocket(); err != nil {
+		die("can't get new req socket: %s", err.Error())
+	}
+	sock.AddTransport(ipc.NewTransport())
+	sock.AddTransport(tcp.NewTransport())
+	if err = sock.Dial(url); err != nil {
+		die("can't dial on req socket: %s", err.Error())
+	}
+	fmt.Printf("NODE1: SENDING DATE REQUEST %s\n", "DATE")
+	if err = sock.Send([]byte("DATE")); err != nil {
+		die("can't send message on push socket: %s", err.Error())
+	}
+	if msg, err = sock.Recv(); err != nil {
+		die("can't receive date: %s", err.Error())
+	}
+	fmt.Printf("NODE1: RECEIVED DATE %s\n", string(msg))
+	sock.Close()
+}
 
 func main() {
-	fmt.Println("main...")
+	if len(os.Args) > 2 && os.Args[1] == "node0" {
+		node0(os.Args[2])
+		os.Exit(0)
+	}
+	if len(os.Args) > 2 && os.Args[1] == "node1" {
+		node1(os.Args[2])
+		os.Exit(0)
+	}
+	if len(os.Args) > 2 && os.Args[1] == "node2" {
+		node1(os.Args[2])
+		os.Exit(0)
+	}
+	fmt.Fprintf(os.Stderr, "Usage: reqrep node0|node1|node2 <URL>\n")
+	os.Exit(1)
 }
+
+//   $ url=tcp://127.0.0.1:40899
+//   $ ./reqrep node0 $url & node0=$! && sleep 1
+//   $ ./reqrep node1 $url
+//   $ kill $node0
+//
