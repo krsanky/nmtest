fb84a68a8024795499f2dc496da8db16b7d02372
paul wisehart <paul@oldcode.org>
2018-04-20 21:11:41 +0000

    cleanup

--- cmd/nmserver/main.go
+++ cmd/nmserver/main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/go-mangos/mangos"
 	"github.com/go-mangos/mangos/protocol/rep"
-	"github.com/go-mangos/mangos/protocol/req"
 	"github.com/go-mangos/mangos/transport/ipc"
 	"github.com/go-mangos/mangos/transport/tcp"
 )
@@ -15,10 +14,6 @@ import (
 func die(format string, v ...interface{}) {
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, v...))
 	os.Exit(1)
-}
-
-func date() string {
-	return time.Now().Format(time.ANSIC)
 }
 
 func server(url string) {
@@ -37,9 +32,8 @@ func server(url string) {
 		// Could also use sock.RecvMsg to get header
 		msg, err = sock.Recv()
 		if string(msg) == "DATE" { // no need to terminate
-			fmt.Println("NODE0: RECEIVED DATE REQUEST")
-			d := date()
-			fmt.Printf("NODE0: SENDING DATE %s\n", d)
+
+			d := time.Now().Format(time.ANSIC)
 			err = sock.Send([]byte(d))
 			if err != nil {
 				die("can't send reply: %s", err.Error())
@@ -48,49 +42,14 @@ func server(url string) {
 	}
 }
 
-func node1(url string) {
-	var sock mangos.Socket
-	var err error
-	var msg []byte
-
-	if sock, err = req.NewSocket(); err != nil {
-		die("can't get new req socket: %s", err.Error())
-	}
-	sock.AddTransport(ipc.NewTransport())
-	sock.AddTransport(tcp.NewTransport())
-	if err = sock.Dial(url); err != nil {
-		die("can't dial on req socket: %s", err.Error())
-	}
-	fmt.Printf("NODE1: SENDING DATE REQUEST %s\n", "DATE")
-	if err = sock.Send([]byte("DATE")); err != nil {
-		die("can't send message on push socket: %s", err.Error())
-	}
-	if msg, err = sock.Recv(); err != nil {
-		die("can't receive date: %s", err.Error())
-	}
-	fmt.Printf("NODE1: RECEIVED DATE %s\n", string(msg))
-	sock.Close()
-}
-
 func main() {
-	if len(os.Args) > 2 && os.Args[1] == "server" {
-		server(os.Args[2])
+	//fmt.Printf("args:%d", len(os.Args))
+	if len(os.Args) < 2 {
+		fmt.Fprintf(
+			os.Stderr, "Usage: nmserver <URL>(tcp://127.0.0.1:40899)\n")
+	} else {
+		server(os.Args[1])
 		os.Exit(0)
 	}
-	if len(os.Args) > 2 && os.Args[1] == "node1" {
-		node1(os.Args[2])
-		os.Exit(0)
-	}
-	if len(os.Args) > 2 && os.Args[1] == "node2" {
-		node1(os.Args[2])
-		os.Exit(0)
-	}
-	fmt.Fprintf(os.Stderr, "Usage: reqrep server|node1|node2 <URL>(tcp://127.0.0.1:40899)\n")
 	os.Exit(1)
 }
-
-//   $ url=tcp://127.0.0.1:40899
-//   $ ./reqrep node0 $url & node0=$! && sleep 1
-//   $ ./reqrep node1 $url
-//   $ kill $node0
-//
